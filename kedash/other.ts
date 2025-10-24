ebbb3eabd1c2b604d4c4b8c732d6a0891f2aaae3
King3 <75344974+coderking3@users.noreply.github.com>
2025-10-24 16:11:18 +0800

    dev

---
+++ kedash/other.ts
@@ -0,0 +1,70 @@
+
+export const getQueryParams = <T extends string>(keys: T[]) => {
+  if (!keys || !Array.isArray(keys) || keys.length === 0) {
+    console.error(
+      `[GetQueryParams Error]: keys must be a non-empty array of strings`
+    )
+    return {} as Record<T, string | undefined>
+  }
+
+  const searchParams = new URLSearchParams(window.location.search)
+  const params = keys.reduce(
+    (res, key) => {
+      const param = searchParams.get(key)
+
+      if (param) {
+        res[key] = param
+      } else {
+        res[key] = undefined
+      }
+
+      return res
+    },
+    {} as Record<T, string | undefined>
+  )
+
+  return params
+}
+
+export const setQueryParams = (params: Record<string, any>) => {
+  if (!params || params?.constructor !== Object) {
+    console.error(`[SetQueryParams Error]: params must be an object`)
+    return
+  }
+
+  const keys = Object.entries(params)
+  if (keys.length === 0) {
+    console.error(`[SetQueryParams Error]: params must be a non-empty object`)
+    return
+  }
+
+  const searchParams = new URLSearchParams(window.location.search)
+  for (const [param, value] of keys) {
+    if (
+      value === null ||
+      value === undefined ||
+      (typeof value === 'string' && !value.trim())
+    ) {
+      continue
+    }
+
+    searchParams.set(param, String(value))
+  }
+
+  const queryString = searchParams.toString()
+  const newUrl = queryString ? `?${queryString}` : window.location.pathname
+
+  window.history.replaceState({}, '', newUrl)
+}
+
+export const tryParse = (str: string, fallback: any = null) => {
+  try {
+    const result = JSON.parse(str)
+    if (result) {
+      return result
+    }
+    return fallback
+  } catch {
+    return fallback
+  }
+}
